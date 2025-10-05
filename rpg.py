e4da2d1fe94eb2cb82fc75012925a794888b6047
Sajmiya-S <sajmiya@gmail.com>
2025-10-05 22:45:21 +0530

    rpg.py

--- rpg.py
+++ rpg.py
@@ -12,16 +12,16 @@ print(f"{player} vs {enemy}\n")
 print(f"{player} Health : {pHealth} \n{enemy} Health : {eHealth} ")
 while eHealth >= 0 and pHealth >= 0:
     print("\nTURN ",turn)
-# AttackByPlayer
+    # AttackByPlayer
     edamage = random.randint(10,30)
     eHealth = eHealth - edamage
     print(f"{enemy} health after attack by {player}: ",eHealth)
-# AttackByEnemy
+    # AttackByEnemy
     pdamage = random.randint(10,35)
     pdamage = pdamage // 2  # Defend
     pHealth = pHealth - pdamage
     print(f"{player} health after attack by {enemy}: ",pHealth)   
-# Heal(Restores player health):
+    # Heal(Restores player health)
     pheal = random.randint(10,25)
     if pHealth + pheal < 100:
         pHealth = pHealth + pheal
@@ -33,3 +33,4 @@ if pHealth <= 0:
     print(f"\nGame over!!!\n{enemy} wins!!!\n")
 elif eHealth <= 0:
     print(f"\nGame over!!!\n{player} wins!!!\n")
+
